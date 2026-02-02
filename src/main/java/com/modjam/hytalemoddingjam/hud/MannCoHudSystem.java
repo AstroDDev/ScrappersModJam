17f2f154b4de11036960633d4e3b473a4cf785aa
Diamssword <hugo.piechowiak@proton.me>
2026-02-02 18:18:15 +0100

    Update MannCoHudSystem.java

--- src/main/java/com/modjam/hytalemoddingjam/hud/MannCoHudSystem.java
+++ src/main/java/com/modjam/hytalemoddingjam/hud/MannCoHudSystem.java
@@ -46,10 +46,13 @@ public class MannCoHudSystem extends EntityTickingSystem<EntityStore> {
 
         UICommandBuilder builder = new UICommandBuilder();
         builder.append("Hud.ui");
-		if(gameLogic.getWaveHelper().isIntermission())
+		if(gameLogic.getWaveHelper().isIntermission()) {
 			builder.set("#GearLabel.Text", "0/0");
+			builder.set("#WaveLabel.Text", "Prep Time");
+		}
 		else {
 			builder.set("#GearLabel.Text", gameLogic.getWaveHelper().getScrapCollectedWave()+"/"+gameLogic.getWaveHelper().getQuota());
+			builder.set("#WaveLabel.Text", "Wave "+(gameLogic.getWaveHelper().getWaveIndex()+1));
 		}
         if (System.currentTimeMillis() - gameLogic.getWaveHelper().getWaveStartTime() < ANNOUNCEMENT_LENGTH_MILLIS) {
             if (gameLogic.getWaveHelper().isIntermission()) {
